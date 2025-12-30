1e1206fd6e5e9f01ee4ff2dc4ddd252f99e590e0
RiteshDev99 <riteshchaudhary5191@gmail.com>
2025-12-31 00:06:33 +0530

    first commit

---
+++ src/latest-Job.ts
@@ -0,0 +1,23 @@
+import * as cheerio from 'cheerio';
+
+async function latestJob(): Promise<void> {
+    const response = await fetch("https://sarkariresult.com.cm/latest-jobs/");
+    const res = await response.text();
+
+    const $ = cheerio.load(res);
+
+    $('ul.latest-posts-last-date li').each(
+        (index: number, element) => {
+            const text = $(element).text().trim();
+            const link = $(element).find('a').attr('href');
+
+            console.log({
+                index,
+                text,
+                link
+            });
+        }
+    );
+}
+
+latestJob();
